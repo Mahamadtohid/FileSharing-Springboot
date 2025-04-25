66488900226dd0f4b83afe7686d866074ada39ad
Mahamadtohid <naikwadimdtohid@gmail.com>
2025-04-25 19:46:09 +0530

    Basic Frontend

---
+++ src/main/java/com/example/FileSharing/controller/FileController.java
@@ -0,0 +1,32 @@
+package com.example.FileSharing.controller;
+
+import java.util.List;
+
+import org.apache.tomcat.jni.FileInfo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping()
+public class FileController {
+
+    @GetMapping("/files")
+    public String login() {
+        return "home";
+    }
+
+    @GetMapping("/list")
+    public String files() {
+        return "list-files"; // 
+    }
+
+    @GetMapping("/share")
+    public String share() {
+        return "share-files";
+    }
+    
+    
+}
